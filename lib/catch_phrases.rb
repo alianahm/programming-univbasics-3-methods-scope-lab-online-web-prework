e4924a2195668f11ba4a5053ce3785efc0bc3d6b
Alianah Marie Magalued <alianahmagalued@yahoo.com>
2019-10-01 03:24:40 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,18 +1,18 @@
 describe "mario" do
   it "puts out 'It's-a me, Mario!'" do
-    expect{mario}.to output("It's-a me, Mario!\n").to_stdout
+    expect{mario}.to output("It's-a me, Mario!\n")
   end
 end
 
 describe "toadstool" do
   it "puts out 'Thank You Mario! But Our Princess Is In Another Castle'" do
-    expect{toadstool}.to output("Thank You Mario! But Our Princess Is In Another Castle!\n").to_stdout
+    expect{toadstool}.to output("Thank You Mario! But Our Princess Is In Another Castle!\n")
   end
 end
 
 describe "any_phrase" do
   it "takes in an argument and puts out the catch phrase" do
     phrase = "Do A Barrel Roll!"
-    expect{any_phrase(phrase)}.to output("Do A Barrel Roll!\n").to_stdout
+    expect{any_phrase(phrase)}.to output("Do A Barrel Roll!\n")
   end
 end
