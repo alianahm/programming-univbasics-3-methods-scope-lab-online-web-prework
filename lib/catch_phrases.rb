facecaec79728dbb2ba20fce3a0bf23d417dadbd
Alianah Marie Magalued <alianahmagalued@yahoo.com>
2019-10-01 03:11:38 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -8,7 +8,4 @@ def toadstool
 end
 describe "mario" do
   it "puts out 'It's-a me, Mario!'" do
-    phrase = "It's-a me, Mario!"
-    expect{mario}.to output("It's-a me, Mario!\n").to_stdout
-  end
-end
+    expect{mario}.to output("It's-a me, Mario!")
