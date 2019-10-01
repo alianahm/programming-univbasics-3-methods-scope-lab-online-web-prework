7de6c4065ae762e57a7101ffb669b7c109014efb
Alianah Marie Magalued <alianahmagalued@yahoo.com>
2019-10-01 03:23:36 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,11 +1,4 @@
-def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
-end
 
-def toadstool
-  puts status
-end
 describe "mario" do
   it "puts out 'It's-a me, Mario!'" do
     expect{mario}.to output("It's-a me, Mario!\n").to_stdout
